cc6bb8a28b4521f6b1ef666689f8aa13c39d7028
Emmanuel Blancas <theassethero@gmail.com>
2026-05-06 17:26:41 -0600

    before refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer";
-import Plugins from "@/components/Plugins";
 import ProfileSection from "@/components/ProfileSection";
 import Showcase from "@/components/Showcase";
 
@@ -7,7 +6,6 @@ export default function Home() {
   return (
     <main className="relative min-h-screen w-screen scroll-smooth bg-black text-white">
       <Showcase />
-      <Plugins />
       <ProfileSection />
       <Footer />
     </main>
